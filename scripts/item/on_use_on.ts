89a460b5eb34352b58f5d33f13ed4d8f31ee4742
SmokeyStack <seansyahril04@gmail.com>
2024-04-20 16:52:20 -0400

    Added fertilizeable

--- scripts/item/on_use_on.ts
+++ scripts/item/on_use_on.ts
@@ -1,6 +1,12 @@
 import {
+    BlockPermutation,
+    BlockStates,
+    Dimension,
     ItemComponentUseOnEvent,
     ItemCustomComponent,
+    ItemStack,
+    Player,
+    Vector3,
     world
 } from '@minecraft/server';
 
@@ -27,3 +33,125 @@ export class debug extends onUseOn {
         world.sendMessage(`Item: ${componentData.itemStack.typeId}`);
     }
 }
+
+class useOnFertilizable extends onUseOn {
+    onUseOn(componentData: ItemComponentUseOnEvent) {
+        if (
+            blockMap
+                .get(componentData.block.typeId)
+                ?.canGrow(
+                    componentData.block.dimension,
+                    componentData.block.location,
+                    componentData.usedOnBlockPermutation
+                ) &&
+            blockMap
+                .get(componentData.block.typeId)
+                ?.isFertilizable(
+                    componentData.block.dimension,
+                    componentData.block.location,
+                    componentData.usedOnBlockPermutation
+                )
+        ) {
+            world.sendMessage('Can grow');
+            blockMap
+                .get(componentData.block.typeId)
+                ?.grow(
+                    componentData.block.dimension,
+                    componentData.block.location,
+                    componentData.usedOnBlockPermutation
+                );
+            let player: Player = componentData.source as Player;
+            let item = player
+                .getComponent('inventory')
+                .container.getItem(player.selectedSlot);
+
+            if (item.amount == 1)
+                player
+                    .getComponent('inventory')
+                    .container.setItem(
+                        player.selectedSlot,
+                        new ItemStack('minecraft:air')
+                    );
+            else
+                player
+                    .getComponent('inventory')
+                    .container.setItem(
+                        player.selectedSlot,
+                        new ItemStack(item.typeId, item.amount - 1)
+                    );
+        }
+    }
+}
+
+interface Fertilizable {
+    isFertilizable(
+        dimension: Dimension,
+        block_position: Vector3,
+        block_permutation: BlockPermutation
+    ): boolean;
+    canGrow(
+        dimension: Dimension,
+        block_position: Vector3,
+        block_permutation: BlockPermutation
+    ): boolean;
+    grow(
+        dimension: Dimension,
+        block_position: Vector3,
+        block_permutation: BlockPermutation
+    ): void;
+}
+
+class cropBlock implements Fertilizable {
+    constructor() {}
+    isFertilizable(
+        dimension: Dimension,
+        block_position: Vector3,
+        block_permutation: BlockPermutation
+    ): boolean {
+        let a: number = BlockStates.get('growth').validValues[
+            BlockStates.get('growth').validValues.length - 1
+        ] as number;
+
+        let b: number = block_permutation.getState('growth') as number;
+
+        return a != b;
+    }
+    canGrow(
+        dimension: Dimension,
+        block_position: Vector3,
+        block_permutation: BlockPermutation
+    ): boolean {
+        return true;
+    }
+
+    grow(
+        dimension: Dimension,
+        block_position: Vector3,
+        block_permutation: BlockPermutation
+    ): void {
+        let a: number = BlockStates.get('growth').validValues[
+            BlockStates.get('growth').validValues.length - 1
+        ] as number;
+        let b: number =
+            (block_permutation.getState('growth') as number) +
+            (Math.random() * 4 + 2);
+        if (b > a) {
+            b = a;
+        }
+        dimension.setBlockPermutation(
+            block_position,
+            BlockPermutation.resolve('minecraft:wheat', { growth: b })
+        );
+        dimension.spawnParticle('minecraft:crop_growth_emitter', {
+            x: block_position.x + 0.5,
+            y: block_position.y + 0.5,
+            z: block_position.z + 0.5
+        });
+    }
+}
+
+const blockMap = new Map<string, Fertilizable>();
+
+const wheat = new cropBlock();
+
+blockMap.set('minecraft:wheat', wheat);
