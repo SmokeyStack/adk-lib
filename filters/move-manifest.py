b90cf9d3b6bd739e768c42de51e25607378c9465
SmokeyStack <seansyahril04@gmail.com>
2024-06-07 12:46:07 -0400

    Added move-manifest filter

---
+++ filters/move-manifest.py
@@ -0,0 +1,9 @@
+import shutil
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv)-1 < 1:
+        print("Too few arguments")
+        exit(1)
+    
+    shutil.copyfile(f'data/{sys.argv[1]}', "BP/manifest.json")
